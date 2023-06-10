f8a598166a4d004b1baacf16fadc2ec8544783ac
Du Cassoulet <81047473+du-cassoulet@users.noreply.github.com>
2023-06-10 13:00:41 +0200

    Delete out directory

--- out/Bot/classes/Event.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Event {
-    constructor(eventName, eventCallback) {
-        this.eventName = eventName;
-        this.eventCallback = eventCallback;
-    }
-}
-exports.default = Event;
--- out/Nlp/functions/sentencize.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sentencize(tokens) {
-    return tokens
-        .join(" ")
-        .replace(/\s(,|\.|:)/g, "$1")
-        .replace(/\s('|-)\s/g, "$1");
-}
-exports.default = sentencize;
